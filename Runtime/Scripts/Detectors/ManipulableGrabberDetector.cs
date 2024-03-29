de2498f2fc82ee3d7e631a033cfe292780d507fa
FrancescoWeik <fweikmann@gmail.com>
2024-03-29 12:50:27 +0100

    update manipulable grabber to receive events

--- Runtime/Scripts/Detectors/ManipulableGrabberDetector.cs
+++ Runtime/Scripts/Detectors/ManipulableGrabberDetector.cs
@@ -1,6 +1,8 @@
 
 using Reflectis.SDK.CreatorKit;
+using Reflectis.SDK.InteractionNew;
 using UnityEngine;
+using UnityEngine.Events;
 using static Reflectis.SDK.InteractionNew.IInteractable;
 
 namespace Reflectis.PLG.TasksReflectis
@@ -8,6 +10,10 @@ namespace Reflectis.PLG.TasksReflectis
     public class ManipulableGrabberDetector : MonoBehaviour
     {
         public InteractablePlaceholder interactablePlaceholder;
+        public UnityEvent OnGrabStart = default;
+        public UnityEvent OnGrabEnd = default;
+
+        //private Grabbable grabbableObj;
 
         // Start is called before the first frame update
         void Start()
@@ -15,8 +21,39 @@ namespace Reflectis.PLG.TasksReflectis
             if (interactablePlaceholder.InteractionModes.HasFlag(EInteractableType.Manipulable))
             {
                 Debug.LogError("This is a maniupulable object and so I have to add the detector when grabbed");
+                interactablePlaceholder.OnSetupFinished.AddListener(OnSetupFinished);
             }
         }
+
+        public void OnSetupFinished()
+        {
+            //gameObject.GetOrAddComponent<Grabbable>();
+
+            //add OnGrabEvent to the Manipulable. The ManipulableVR and ManipulableDesktop will have a callback on that event
+            Manipulable manipulable = interactablePlaceholder.gameObject.GetComponent<Manipulable>();
+            if (manipulable)
+            {
+                Debug.LogError("ok the object has now a manipulable after the setup!!!!");
+                Debug.LogError(manipulable);
+                //Debug.LogError(manipulable.OnGrabManipulableStart);
+                //manipulable.OnGrabManipulableStart.AddListener(OnGrabStartEvent);
+
+            }
+        }
+
+        private void OnGrabStartEvent()
+        {
+            //OnGrabStart.Invoke();
+
+            Debug.LogError("WORKED START EVENT ON MANIPULABLE");
+        }
+
+        private void OnGrabEndEvent()
+        {
+            OnGrabEnd.Invoke();
+            Debug.LogError("WORKED END EVENT ON MANIPULABLE");
+        }
+
     }
 }
 
