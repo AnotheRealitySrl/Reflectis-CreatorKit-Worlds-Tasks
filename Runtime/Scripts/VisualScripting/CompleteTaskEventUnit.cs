9a3d5a8ef181399d9a19bcac3cbbffee538314e3
FrancescoWeik <fweikmann@gmail.com>
2024-05-17 17:12:05 +0200

    Add taskComplete node to taskReflectis

---
+++ Runtime/Scripts/VisualScripting/CompleteTaskEventUnit.cs
@@ -0,0 +1,39 @@
+using Reflectis.PLG.Tasks;
+using Unity.VisualScripting;
+
+namespace Reflectis.PLG.TasksReflectis
+{
+    public class CompleteTaskEventUnit : Unit
+    {
+
+        protected GraphReference graphReference;
+
+        protected Task taskReference; //should simply create the vent taskComplete to complete the task howevereI want yeah
+
+        [DoNotSerialize]
+        public ValueInput TaskToComplete { get; private set; }
+
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ControlInput inputTrigger { get; private set; }
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ControlOutput outputTrigger { get; private set; }
+
+        protected override void Definition()
+        {
+            inputTrigger = ControlInput(nameof(inputTrigger), Output);
+            outputTrigger = ControlOutput("outputTrigger");
+            TaskToComplete = ValueInput<Task>(nameof(TaskToComplete), null).NullMeansSelf();
+            Succession(inputTrigger, outputTrigger);
+
+        }
+
+        private ControlOutput Output(Flow flow)
+        {
+            flow.GetValue<Task>(TaskToComplete).CompleteTask();
+            return outputTrigger;
+        }
+    }
+
+}
