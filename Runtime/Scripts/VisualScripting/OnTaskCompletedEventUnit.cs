0e48a2e3a175bf2e5a4fc4c1efd81f8445adf62c
FrancescoWeik <fweikmann@gmail.com>
2024-05-20 10:36:09 +0200

    Add OnTaskComplete node

---
+++ Runtime/Scripts/VisualScripting/OnTaskCompletedEventUnit.cs
@@ -0,0 +1,50 @@
+using Reflectis.PLG.Tasks;
+using Unity.VisualScripting;
+
+namespace Reflectis.PLG.TasksReflectis
+{
+    public class OnTaskCompletedEventUnit : EventUnit<Task>
+    {
+        protected override bool register => true;
+
+        protected GraphReference graphReference;
+
+        protected Task taskReference;
+
+        [DoNotSerialize]
+        public ValueInput TaskReference { get; private set; }
+
+        protected override void Definition()
+        {
+            base.Definition();
+            TaskReference = ValueInput<Task>(nameof(TaskReference), null).NullMeansSelf();
+        }
+
+        public override EventHook GetHook(GraphReference reference)
+        {
+            graphReference = reference;
+            using (var flow = Flow.New(reference))
+            {
+                RegisterTaskOnCompleted(flow);
+            }
+            return new EventHook("Task" + this.ToString().Split("EventUnit")[0]);
+        }
+
+        private void RegisterTaskOnCompleted(Flow flow)
+        {
+            taskReference = flow.GetValue<Task>(TaskReference);
+            if (taskReference == null)
+            {
+            }
+            else
+            {
+                taskReference.OnTaskCompleted.AddListener(OnTaskCompletedUnit);
+            }
+        }
+
+        private void OnTaskCompletedUnit(bool value)
+        {
+            Trigger(graphReference, taskReference);
+        }
+    }
+}
