983ff6b998ba5c609812a9fe2a195d0b972c6178
Francesco Weikmann <fweikmann@gmail.com>
2024-07-09 17:52:35 +0200

    Add TaskReflectisStepSetter

---
+++ Runtime/Scripts/Core/TaskReflectisStepSetter.cs
@@ -0,0 +1,68 @@
+using Reflectis.PLG.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Reflectis.PLG.TasksReflectis
+{
+    public class TaskReflectisStepSetter : TaskStepSetter
+    {
+        protected ITasksRPCManager rpcManagerInterface;
+
+        private void Start()
+        {
+            if (TaskSystemReflectis.Instance.isNetworked)
+            {
+                StartCoroutine(WaitForRPCManager());
+                taskSystem = TaskSystemReflectis.Instance;
+            }
+            
+        }
+
+        private IEnumerator WaitForRPCManager(){
+            while (rpcManagerInterface == null)
+            {
+                rpcManagerInterface = TaskSystemReflectis.Instance.rpcManagerInterface;
+                yield return null;
+            }
+            rpcManagerInterface = TaskSystemReflectis.Instance.rpcManagerInterface;
+            rpcManagerInterface.AddJoinRoomEvent(Init);
+        }
+
+
+
+        private void Init(int id)
+        {
+            //calculate last node
+            var tasks = FindObjectsOfType<TaskReflectis>();
+            TaskNode targetNode = null;
+            foreach (var task in tasks)
+            {
+                if (task.TaskID == id)
+                {
+                    targetNode = task.Node;
+                    break;
+                }
+            }
+
+            TaskNode newNode = targetNode;
+            if (targetNode.Dependencies.Count != 0 && targetNode.Next != null)
+            {
+                newNode = targetNode.Next;
+            }
+
+            // Ordered list of tasks. I assign the state "Complete" until I find the node I want.
+            IReadOnlyCollection<TaskNode> allNodes = TaskSystemReflectis.Instance.Tasks;
+            foreach (TaskNode node in allNodes)
+            {
+                if (CompleteTaskRecursive(node, newNode))
+                {
+                    // Target reached. Nothing else to do in this loop.
+                    if (targetNode.Status == TaskNode.TaskStatus.Todo)
+                        targetNode.Status = TaskNode.TaskStatus.Completed;
+                    break;
+                }
+            }
+        }
+    }
+}
