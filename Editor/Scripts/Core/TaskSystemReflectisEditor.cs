ddc9845d712f23dff5164d79dfd95fcda5f679ae
Riccardo Verza <feelthecabbage@gmail.com>
2024-07-11 18:50:59 +0200

    Added revert test button to TaskSystemReflectisEditor inspector.

---
+++ Editor/Scripts/Core/TaskSystemReflectisEditor.cs
@@ -0,0 +1,9 @@
+﻿using Reflectis.PLG.TasksReflectis;
+using UnityEditor;
+using UnityEngine;
+
+namespace Reflectis.PLG.TasksEditor
+{
+    [CustomEditor(typeof(TaskSystemReflectis))]
+    public class TaskSystemReflectisEditor : TaskSystemEditor { }
+}
