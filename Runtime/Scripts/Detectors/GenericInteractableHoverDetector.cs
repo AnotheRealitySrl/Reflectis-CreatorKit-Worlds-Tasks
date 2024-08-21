532a4e86bab91b8e2b99ba748f11c260fe77aace
Lucas <>
2024-08-21 12:38:01 +0200

    Add new detector

---
+++ Runtime/Scripts/Detectors/GenericInteractableHoverDetector.cs
@@ -0,0 +1,100 @@
+using Reflectis.SDK.CreatorKit;
+using Reflectis.SDK.InteractionNew;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Reflectis.SDK.InteractionNew.IInteractable;
+using UnityEngine.Events;
+
+namespace Reflectis.PLG.TasksReflectis
+{
+    public class GenericInteractableHoverDetector : MonoBehaviour
+    {
+        public InteractablePlaceholder interactablePlaceholder;
+        [SerializeField]
+        private bool allowRay;
+        [SerializeField]
+        private bool allowHandDirect;
+        [SerializeField]
+        private bool allowMouse;
+
+        public UnityEvent OnHoverEnter = default;
+        public UnityEvent OnHoverExit = default;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (interactablePlaceholder.InteractionModes.HasFlag(EInteractableType.GenericInteractable))
+            {
+                interactablePlaceholder.OnSetupFinished.AddListener(OnSetupFinished);
+            }
+        }
+
+        public void OnSetupFinished()
+        {
+            //add OnGrabEvent to the Manipulable. The ManipulableVR and ManipulableDesktop will have a callback on that event
+            GenericInteractable interactable = interactablePlaceholder.gameObject.GetComponent<GenericInteractable>();
+
+            if (interactable)
+            {
+                if (allowHandDirect)
+                {
+                    interactable.OnHoverGrabEnter.AddListener(OnHoverEnterEvent);
+                    interactable.OnHoverGrabExit.AddListener(OnHoverExitEvent);
+                }
+
+                if (allowRay)
+                {
+                    interactable.OnHoverRayEnter.AddListener(OnHoverEnterEvent);
+                    interactable.OnHoverRayExit.AddListener(OnHoverExitEvent);
+                }
+
+                if (allowMouse)
+                {
+                    interactable.OnHoverMouseEnter.AddListener(OnHoverEnterEvent);
+                    interactable.OnHoverMouseExit.AddListener(OnHoverExitEvent);
+                }
+            }
+        }
+
+        public void OnEnable()
+        {
+            OnSetupFinished();
+        }
+
+        public void OnDisable()
+        {
+            GenericInteractable interactable = interactablePlaceholder.gameObject.GetComponent<GenericInteractable>();
+            if (interactable)
+            {
+                if (allowHandDirect)
+                {
+                    interactable.OnHoverGrabEnter.RemoveListener(OnHoverEnterEvent);
+                    interactable.OnHoverGrabExit.RemoveListener(OnHoverExitEvent);
+                }
+
+                if (allowRay)
+                {
+                    interactable.OnHoverRayEnter.RemoveListener(OnHoverEnterEvent);
+                    interactable.OnHoverRayExit.RemoveListener(OnHoverExitEvent);
+                }
+
+                if (allowMouse)
+                {
+                    interactable.OnHoverMouseEnter.RemoveListener(OnHoverEnterEvent);
+                    interactable.OnHoverMouseExit.RemoveListener(OnHoverExitEvent);
+                }
+            }
+        }
+
+        private void OnHoverEnterEvent()
+        {
+            OnHoverEnter.Invoke();
+        }
+
+        private void OnHoverExitEvent()
+        {
+            OnHoverExit.Invoke();
+        }
+    }
+}
